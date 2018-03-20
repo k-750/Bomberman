5ba82385f19ebd48f7f9d7f19c802932b3b30567
Pan i Wladca <paniwladca@malpa.zsrr>
2018-03-20 18:10:56 +0100

    Roboczy

--- OknoGlowne.java
+++ OknoGlowne.java
@@ -1,5 +1,8 @@
 import java.awt.Color;
+import java.awt.Container;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -10,9 +13,6 @@ public class OknoGlowne extends JFrame
 	
 	private int iloscPunktow;
 	
-	/**
-	 * wyświetla ilość punktów 	
-	 */
 	private JLabel labelPunkty;
 	
 	/**
@@ -25,49 +25,45 @@ public class OknoGlowne extends JFrame
 	 */
 	JPanel panelDolny;
 	
-
+	/**
+	 * tabela min
+	 */
+	private JButton[] pola = new JButton[100];
+	
 	public OknoGlowne()
 	{
 		super("Bomberman");
-		setSize(800,700);
+		setSize(650,500);
 		setLocation(200,50);
 		setLayout(null);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		panelGorny = new JPanel();
-		panelGorny.setBounds(10,10,780,30);
+		panelGorny = new JPanel();				// panel z punktami
+		panelGorny.setBounds(10,10,630,30);
 		panelGorny.setBackground(Color.WHITE);
 			labelPunkty = new JLabel("Punkty: "+iloscPunktow);
 			panelGorny.add(labelPunkty);
-		add(panelGorny);
-		
-		panelDolny = new JPanel();
-		panelDolny.setBounds(10,80,780,600);
+		this.add(panelGorny);
+			
+		panelDolny = new JPanel();			// panel z minami
+		panelDolny.setBounds(10,60,630,430);
 		panelDolny.setBackground(new Color(200,230,240));
-		add(panelDolny);
-		
-		
+		panelDolny.setLayout(new GridLayout(10,10,1,1));
+			
+			for (int i = 0; i<100; i++)
+			{
+				pola[i] = new JButton(" ");
+				panelDolny.add(pola[i]);
+			}
+
+		this.add(panelDolny);	
 	}
 
-	
 	public static void main(String[] args)
 	{
 		OknoGlowne oknoGlowne = new OknoGlowne();
 
 	}
 	
-	
-	public int getIloscPunktow()
-	{
-		return iloscPunktow;
-	}
-
-	public void setIloscPunktow(int iloscPunktow)
-	{
-		this.iloscPunktow = iloscPunktow;
-	}
-	
-	
-
 }
